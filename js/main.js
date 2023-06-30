e351717b97508b94e5ec9b3794deaf93f87de79b
stefanopriolo <stefanojpriolo@gmail.com>
2023-06-30 16:54:06 +0200

    esercizio

--- js/main.js
+++ js/main.js
@@ -4,21 +4,28 @@ let price = km * 0.21;
 let discountUnder = ((price * 20) / 100);
 let discountOver = ((price * 40) / 100);
 
-console.log (totalPrice)
-console.log( typeof km);
-console.log( typeof age);
-console.log( typeof discountUnder);
-console.log( typeof discountOver);
-console.log( typeof totalPrice);
+console.log( km);
+console.log( age);
 
-if age < 18 {
-    const totalPrice = price - discountUnder
+
+if (age < 18) {
+    const totalPrice = (price - discountUnder)
     console.log("sei minorenne, hai diritto a uno sconto del 20%")
+    console.log (Math.round(totalPrice))
+} else {
+    totalPrice = price
+    console.log (Math.round(totalPrice))
 }
 
-if age > 65 {
-    const totalPrice = price - discountOver
+if (age > 65) {
+    const totalPrice = (price - discountOver)
     console.log("sei over 65, hai diritto a uno sconto del 40%")
+    console.log (Math.round(totalPrice))
+}  else {
+    totalPrice = price
+    console.log (Math.round(totalPrice))
 }
+
 
 document.getElementById("totalPrice").innerHTML = totalPrice;
+document.getElementById("sei minorenne, hai diritto a uno sconto del 20%").innerHTML = totalPrice;
